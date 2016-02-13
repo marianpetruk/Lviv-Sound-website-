02a908452d87ba7f0e04346f837c72117400f08f
Marian Petruk <marian.petruk@gmail.com>
2016-02-13 22:06:57 +0200

    Update site
    Major update+radio functional

--- javascript/assets/application.js
+++ javascript/assets/application.js
@@ -1,26 +1,41 @@
 $(function() {
 
-  /*-----------------------------------------------------------------------------------*/
-  /*  Anchor Link
-  /*-----------------------------------------------------------------------------------*/
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-      || location.hostname == this.hostname) {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
-      var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-        return false;
-      }
-    }
-  });
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 
-  /*-----------------------------------------------------------------------------------*/
-  /*  Tooltips
-  /*-----------------------------------------------------------------------------------*/
-  $('.tooltip-side-nav').tooltip();
-  
+    $('.tooltip-side-nav').tooltip();
+
+});
+
+$(document).ready(function() {
+    $(".fancybox-effects-d").fancybox({
+        padding: 0,
+        autoSize: true,
+        overlayShow: true,
+        fitToView: false,
+        maxWidth: 940,
+
+        openEffect: 'elastic',
+        openSpeed: 150,
+
+        closeEffect: 'elastic',
+        closeSpeed: 150,
+        closeClick: true,
+
+        helpers: {
+            overlay: null
+        }
+    });
+
 });
